a717e2766939b7ef7414a7ffcd0b288f97c08740
Valentyn Vasyljanskiy <vasiljanskiyy@gmail.com>
2026-03-26 14:00:35 +0200

    Update favicon and enhance document head with additional icon links; improve user experience with proper favicon support and metadata for web applications.

--- src/components/ErrorPageTemplate.tsx
+++ src/components/ErrorPageTemplate.tsx
@@ -261,3 +261,5 @@ export function ErrorPageTemplate({
   )
 }
 
+
+
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,21 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0&display=swap"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <Main />
